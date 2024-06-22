6e8546217196a438c91bf11a5470aa384ac442b7
saifmohammad00 <saifmohammad592001@gmail.com>
2024-06-22 21:47:33 +0530

    date formatted

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
 import "./ExpenseItem.css";
-
-function ExpenseItem(props) {
+const months=['January','February','March','April','May','June','July','August','September','October','November','December'];
+function ExpenseItem(props){
+  const month=months[props.date.getMonth()];
+  const year=props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__location">{props.location}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
@@ -12,5 +18,4 @@ function ExpenseItem(props) {
     </div>
   );
 }
-
 export default ExpenseItem;
