0e0b76794637ddcf48aab9604293a0695aecb868
saifmohammad00 <saifmohammad592001@gmail.com>
2024-06-22 22:35:41 +0530

    splitting components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
 import "./ExpenseItem.css";
-const months=['January','February','March','April','May','June','July','August','September','October','November','December'];
-function ExpenseItem(props){
-  const month=months[props.date.getMonth()];
-  const year=props.date.getFullYear();
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
@@ -18,4 +12,5 @@ function ExpenseItem(props){
     </div>
   );
 }
+
 export default ExpenseItem;
