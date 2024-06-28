ba575b9e02bb189ed3bee49f490244f76d98af7a
saifmohammad00 <saifmohammad592001@gmail.com>
2024-06-28 22:17:12 +0530

    Render Expense Form Conditionally

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -36,40 +36,43 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
-  const ab=<div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            id="title"
-            onChange={titleChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label htmlFor="amount">Amount</label>
-          <input
-            type="number"
-            value={enteredAmount}
-            id="amount"
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            value={enteredDate}
-            id="date"
-            min="2023-01-01"
-            max="2024-12-31"
-            onChange={dateChangeHandler}
-          />
-        </div>
-     <div className="new-expense__actions">
-        <button type="submit">Cancel</button>
+  const ab=(<div>
+       <div className="new-expense__controls">
+              <div className="new-expense__control">
+                <label htmlFor="title">Title</label>
+                <input
+                  type="text"
+                  value={enteredTitle}
+                  id="title"
+                  onChange={titleChangeHandler}
+                />
+              </div>
+              <div className="new-expense__control">
+                <label htmlFor="amount">Amount</label>
+                <input
+                  type="number"
+                  value={enteredAmount}
+                  id="amount"
+                  onChange={amountChangeHandler}
+                />
+              </div>
+              <div className="new-expense__control">
+                <label htmlFor="date">Date</label>
+                <input
+                  type="date"
+                  value={enteredDate}
+                  id="date"
+                  min="2023-01-01"
+                  max="2024-12-31"
+                  onChange={dateChangeHandler}
+                />
+              </div>
+            </div>
+      <div className="new-expense__actions">
+           <button type="submit">Cancel</button>
       </div>
       </div>
+      )
    
   
   return (
