7e5e9f29abdc2972656689d4a286cdbbf763ff77
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-02 13:25:29 +0530

    Reusable Button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import "./AddUser.css";
+
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
-  return (<Card className="input">
-    <form onSubmit={addUserHandler} >
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+  return (
+    <Card className="input">
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
     </Card>
   );
 };
