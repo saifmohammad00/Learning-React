f0deda365195a721140574e0500c41bf437015ae
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-02 14:05:02 +0530

    Validating User Inputs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,33 +1,51 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import "./AddUser.css";
 
 const AddUser = () => {
-  const [enteredUser, setUserName]=useState("");
-  const [enteredAge, setAge]=useState("");
-  const handleUserNameChange=(e)=>{
-    setUserName(e.target.value);
-  }
-  const handleAgeChange=(e)=>{
-    setAge(e.target.value);
-  }
-  
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredUser,enteredAge);
-    event.target.reset();
+    if(enteredUsername.trim().length===0 || +enteredAge<=0 || enteredAge.trim().length===0){
+      return ;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className="input">
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={handleUserNameChange}/>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" onChange={handleAgeChange}/>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
 };
+
 export default AddUser;
